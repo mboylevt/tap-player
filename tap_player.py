409d988d58f4233a1e2f2ca9ca82ade4f2e48685
Matthew Boyle <matt@Matthews-MacBook-Pro.local>
2017-03-24 10:36:38 -0400

    add random track selection

--- tap_player.py
+++ tap_player.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, render_template
 import pygame
 import time
+import random
 
 # Not ideal, but since it's just being used across a home network not concerned at the moment.
 app = Flask(__name__, static_url_path='/static')
@@ -16,8 +17,11 @@ def load_home():
 
 @app.route('/play')
 def play_track():
+    tracks = os.listdir('media/')
+    track = 'media/' + random.choice(tracks)
+    print track
     pygame.mixer.init()
-    pygame.mixer.music.load('media/sandman5.ogg')
+    pygame.mixer.music.load(track)
     pygame.mixer.music.play()
     return render_template('play.html')
 
